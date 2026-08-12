c2e2f6b52139892dcc7e231a642b208f18c1c55a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-08-12 16:59:19 -0300

    feat: finish the testing phase

--- src/lab/checkbox.js
+++ src/lab/checkbox.js
@@ -1,4 +1,6 @@
 import { checkbox, Separator } from "@inquirer/prompts"
+import ansiColors from "ansi-colors"
+
 const pkgs = ["react", "node", "nodemon"]
 const ws = [
   {
@@ -18,21 +20,54 @@ const ws = [
   },
 ]
 
-  const combine = pkgs.reduce((acc, curr, index)=>{  
-     const mixed = ws.map((w)=>{
-       w = { name: w.name, value: [w.value, curr].flat()  }
-       return w
+   const combine = pkgs.reduce((acc, curr, index)=>{  
+      const mixed = ws.map((w)=>{
+        w = { name: w.name, value: [w.value, curr].flat()  }
+        return w
+       })
+        acc.push(new Separator(ansiColors.bgCyan(curr.toUpperCase())),...mixed)
+        return acc
+    },[]) 
+
+  
+  const question = await checkbox({
+    message: 'Select any choice',
+    choices: combine,
+    loop: false,
+    validate: async(answers)=>{
+      const outsider = []
+       answers.map((a)=>{
+          outsider.push(a.value[1])
+       })
+      const isNotExistOnValues = pkgs.find((value, index, array)=>{
+           const testing = outsider.includes(value)
+           return !testing
       })
-       acc.push(new Separator(curr.toUpperCase()),...mixed)
-       return acc
-   },[]) 
+      if(isNotExistOnValues){
+        return `You miss select choices on ${isNotExistOnValues} package`
+      }
+      return true
+      },
+    theme:{
+      style:{
+        renderSelectedChoices:(selectedChoices)=> { return 'Done' },
+        key: (text)=> {console.log(); return 'clear';}
+      },
+    
+    }
+  })   
 
 
-const question = await checkbox({
-  message: 'Select any choice',
-  choices: combine,
-  loop: false
-})   
+//   console.log(outsider)
+// const arr = [ 'react', 'react', 'node', 'node']
 
+// const verify = pkgs.find((value, index, array)=>{
+//        const testing = arr.includes(value)
+//        return !testing
+//     })
 
-console.log(question)
+// if(verify){
+//   console.log(`This value [${verify}] doesn't existe on array`)
+// }else{
+//   console.log('All values are incorporated...')
+// }
